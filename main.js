bdfae9a2c4b06cbe1d51dd4c88bba3b887cb3b24
Lukáš Kollár <lukas.kollar@gmail.com>
2025-05-06 09:53:18 +0200

    test

---
+++ main.js
@@ -0,0 +1,380 @@
+import { createApp } from 'https://unpkg.com/petite-vue?module'
+
+createApp({
+    teams: [
+        {
+            "name": "Anaheim Ducks",
+            "shortcut": "ANA",
+            "logo": "ANA.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Utah Hockey Club",
+            "shortcut": "UTA",
+            "logo": "UTA.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Boston Bruins",
+            "shortcut": "BOS",
+            "logo": "BOS.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Buffalo Sabres",
+            "shortcut": "BUF",
+            "logo": "BUF.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Calgary Flames",
+            "shortcut": "CGY",
+            "logo": "CGY.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Carolina Hurricanes",
+            "shortcut": "CAR",
+            "logo": "CAR.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Chicago Blackhawks",
+            "shortcut": "CHI",
+            "logo": "CHI.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Colorado Avalanche",
+            "shortcut": "COL",
+            "logo": "COL.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Columbus Blue Jackets",
+            "shortcut": "CBJ",
+            "logo": "CBJ.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Dallas Stars",
+            "shortcut": "DAL",
+            "logo": "DAL.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Detroit Red Wings",
+            "shortcut": "DET",
+            "logo": "DET.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Edmonton Oilers",
+            "shortcut": "EDM",
+            "logo": "EDM.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Florida Panthers",
+            "shortcut": "FLA",
+            "logo": "FLA.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Los Angeles Kings",
+            "shortcut": "LAK",
+            "logo": "LAK.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Minnesota Wild",
+            "shortcut": "MIN",
+            "logo": "MIN.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Montreal Canadiens",
+            "shortcut": "MTL",
+            "logo": "MTL.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Nashville Predators",
+            "shortcut": "NSH",
+            "logo": "NSH.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "New Jersey Devils",
+            "shortcut": "NJD",
+            "logo": "NJD.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "New York Islanders",
+            "shortcut": "NYI",
+            "logo": "NYI.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "New York Rangers",
+            "shortcut": "NYR",
+            "logo": "NYR.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Ottawa Senators",
+            "shortcut": "OTT",
+            "logo": "OTT.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Philadelphia Flyers",
+            "shortcut": "PHI",
+            "logo": "PHI.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Pittsburgh Penguins",
+            "shortcut": "PIT",
+            "logo": "PIT.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "San Jose Sharks",
+            "shortcut": "SJS",
+            "logo": "SJS.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Seattle Kraken",
+            "shortcut": "SEA",
+            "logo": "SEA.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "St. Louis Blues",
+            "shortcut": "STL",
+            "logo": "STL.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Tampa Bay Lightning",
+            "shortcut": "TBL",
+            "logo": "TBL.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Toronto Maple Leafs",
+            "shortcut": "TOR",
+            "logo": "TOR.png",
+            "conference": "E",
+            "division": "A",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Vancouver Canucks",
+            "shortcut": "VAN",
+            "logo": "VAN.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Vegas Golden Knights",
+            "shortcut": "VGK",
+            "logo": "VGK.png",
+            "conference": "W",
+            "division": "P",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Washington Capitals",
+            "shortcut": "WSH",
+            "logo": "WSH.png",
+            "conference": "E",
+            "division": "M",
+            "points": 0,
+            "clinched": false
+        },
+        {
+            "name": "Winnipeg Jets",
+            "shortcut": "WPG",
+            "logo": "WPG.png",
+            "conference": "W",
+            "division": "C",
+            "points": 0,
+            "clinched": false
+        }
+    ],
+    mounted() {
+        this.generatePoints()
+        this.getPlayoffTeams()
+    },
+    standings: {
+        W: {
+            C: [],
+            P: [],
+            wildCard: []
+        },
+        E: {
+            M: [],
+            A: [],
+            wildCard: []
+        },
+    },
+    getPlayoffTeams() {
+        const divisions = ['A', 'C', 'M', 'P']
+        const eastDivisions = ['A', 'M']
+        const westDivisions = ['C', 'P']
+    
+        // Inicializácia standings objektu
+        this.standings = {
+            W: {
+                C: [],
+                P: [],
+                wildCard: []
+            },
+            E: {
+                M: [],
+                A: [],
+                wildCard: []
+            },
+        }
+    
+        // 1. Zoskupiť tímy podľa divízií
+        const grouped = {
+            A: [],
+            C: [],
+            M: [],
+            P: []
+        }
+    
+        this.teams.forEach(team => {
+            const div = team.division
+            if (grouped[div]) grouped[div].push(team)
+        })
+    
+        // 2. Zoradiť každú divíziu podľa bodov
+        for (const div of divisions) {
+            grouped[div].sort((a, b) => b.points - a.points)
+        }
+    
+        // 3. Pridať top 3 do standings.division
+        for (const div of eastDivisions) {
+            this.standings.E[div] = grouped[div].slice(0, 3) // Top 3 pre východné divízie
+        }
+        for (const div of westDivisions) {
+            this.standings.W[div] = grouped[div].slice(0, 3) // Top 3 pre západné divízie
+        }
+    
+        // 4. Získať zvyšné tímy pre wildCard
+        const eastRemaining = [
+            ...grouped.A.slice(3),
+            ...grouped.M.slice(3)
+        ]
+        const westRemaining = [
+            ...grouped.C.slice(3),
+            ...grouped.P.slice(3)
+        ]
+    
+        // 5. Zoradiť a pridať tímy do wildCard (2 divoké karty na konferenciu)
+        eastRemaining.sort((a, b) => b.points - a.points)
+        westRemaining.sort((a, b) => b.points - a.points)
+    
+        this.standings.E.wildCard = eastRemaining // Pridaj wild card pre východ
+        this.standings.W.wildCard = westRemaining // Pridaj wild card pre západ
+    },    
+    randomNum(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    generatePoints() {
+        this.teams.map(team => {
+            team.points = this.randomNum(50, 120)
+        })
+    }
+}).mount("#nhl-standings")
